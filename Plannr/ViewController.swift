4a0862caa8364440b27e84e679f5d5f4af65009c
ChopperTheKing <rkissos@me.com>
2023-06-25 14:32:58 -0400

    Initial Commit

---
+++ Plannr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Plannr
+//
+//  Created by Ronnie Kissos on 6/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
