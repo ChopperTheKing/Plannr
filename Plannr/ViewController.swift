385dd51c3e6d955cea4f4f7536776cd338474a38
ChopperTheKing <rkissos@me.com>
2023-06-26 08:52:48 -0400

    add calendar view

--- Plannr/ViewController.swift
+++ Plannr/ViewController.swift
@@ -1,19 +1,38 @@
-//
-//  ViewController.swift
-//  Plannr
-//
-//  Created by Ronnie Kissos on 6/25/23.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        createCalendar()
     }
 
+    func createCalendar() {
+        let calendarView = UICalendarView()
+        calendarView.translatesAutoresizingMaskIntoConstraints = false
 
+        calendarView.calendar = .current
+        calendarView.locale = .current
+        calendarView.fontDesign = .rounded
+        calendarView.delegate = self
+        calendarView.layer.cornerRadius = 12
+        view.addSubview(calendarView)
+
+        NSLayoutConstraint.activate([
+            calendarView.leadingAnchor.constraint(equalTo: view.leadingAnchor,
+                constant: 10),
+            calendarView.trailingAnchor.constraint(equalTo: view.trailingAnchor,
+                constant: -10),
+            calendarView.heightAnchor.constraint(equalToConstant:300),
+            calendarView.topAnchor.constraint (equalTo:
+                view.safeAreaLayoutGuide.topAnchor)
+        ])
+    }
 }
 
+extension ViewController: UICalendarViewDelegate {
+    func calendarView(_ calendarView: UICalendarView, decorationFor dateComponents:
+        DateComponents) -> UICalendarView.Decoration? {
+        return nil
+    }
+}
